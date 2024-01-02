4c83d254b929be482d199568e2640a1837575885
Asir Adnan <106885381+asiradnan@users.noreply.github.com>
2024-01-02 22:14:46 +0600

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,20 +1,53 @@
-const getAPI = async(lat,lon,part="hourly") => {
-    console.log(lon);
-    const response = await fetch('https://cors-anywhere.herokuapp.com/'+"https://api.openweathermap.org/weather/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude="+part+"&appid=dbe87d480de539a5aa17ec9f6754d7bc");
+let result = document.getElementById('res');
+const getAPI = async(lat,lon) => {
+    let APIkey = 'dbe87d480de539a5aa17ec9f6754d7bc';
+    latlon = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric`;
+    const response = await fetch(latlon);
     const data = await response.json();
     return data;
 }
-navigator.geolocation.getCurrentPosition(x);
-function x(pos) {
-    let lat=pos.coords.latitude;
-    let lon=pos.coords.longitude;
-    getAPI(lat,lon)
+function loc(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(x);
+        function x(pos) {
+            let lat=pos.coords.latitude;
+            let lon=pos.coords.longitude;
+            getAPI(lat,lon)
+                .then(show)
+                .catch(err => result.innerHTML='Location was not found')
+        }
+    }
+    else {result.innerHTML="Location is not available";}
+    
+}
+
+function city(){
+    byCity(document.getElementById('c').value)
         .then(show)
-        .catch(err => console.log(err.message))
+        .catch(err => result.innerHTML='Location was not found')
 }
-
-
-function show(data){ 
+const byCity = async(cityName) => {
+    let APIkey = 'dbe87d480de539a5aa17ec9f6754d7bc';
+    let xy= `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=metric`;
+    const response = await fetch(xy);
+    const data = await response.json();
+    return data;
+}
+function show(data){
+    result.innerHTML=`Temperature: ${data.main.temp}&degC<br>Feels Like: ${data.main.feels_like}&degC<br>You are currently at ${data.name}.`;
     console.log(data);
-    document.getElementById('main').innerHTML=data.main.temp; 
+    initMap(data.coord.lat,data.coord.lon);
+    
 }
+function initMap(latitude, longitude) {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: { lat: latitude, lng: longitude }
+    });
+  
+    new google.maps.Marker({
+      position: { lat: latitude, lng: longitude },
+      map: map,
+      title: "Your Location"
+    });
+  }
